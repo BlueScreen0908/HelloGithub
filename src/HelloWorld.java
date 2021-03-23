a8f763771cf78e9cc39ac7423fa356708080025b
BlueScreen <1002674945@qq.com>
2021-03-23 22:42:39 +0800

    ...

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("2222");
-        System.out.println("cccc");
     }
 }
